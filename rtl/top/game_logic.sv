6e9e74e1f9f1c0e16c1db7b35f38b0c40a591816
younghyun0702 <younghyun0702@gmail.com>
2026-09-03 15:11:28 +0900

    game_logic / add w_song_bpm

--- rtl/top/game_logic.sv
+++ rtl/top/game_logic.sv
@@ -108,6 +108,7 @@ module game_logic (
         .i_vsync(i_vsync),
         .pattern_state(w_pattern_state),
         .i_pattern_tick(w_pattern_tick),
+        .i_pc_song_bpm(w_song_bpm),
         .stick_control_ready(stick_control_ready),
         .stick_control_valid(stick_control_valid),
         .o_pattern_tick(w_score_pattern_tick),
